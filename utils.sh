fbd0adcf94850cf3e627d86285c817d99d88ed11
oceluspro <contact@ftnl.fr>
2026-03-26 17:46:51 +0100

    Remove unused `inject_github_token` function from `utils.sh`.

--- utils.sh
+++ utils.sh
@@ -13,12 +13,3 @@ safe_branch_name() {
     echo "$1" | tr '[:upper:]' '[:lower:]' | sed -e 's/[^a-z0-9]/-/g' -e 's/^-//' -e 's/-$//'
 }
 
-inject_github_token() {
-    repo_url="$1"
-    if [ -n "$GIT_TOKEN" ] && echo "$repo_url" | grep -qE '^https://github.com/'; then
-        echo "$repo_url" | sed "s|^https://github.com/|https://$GIT_TOKEN@github.com/|"
-    else
-        echo "$repo_url"
-    fi
-}
-
