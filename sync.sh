09de66eb9afa9c52b18cd05ed2dcd97e0a41ba47
oceluspro <contact@ftnl.fr>
2026-03-26 19:49:29 +0100

    Remove dynamic Nginx configuration generation: Delete `nginx_conf_gen.sh`, adjust `sync.sh` logic, and simplify `nginx.conf.template` and Dockerfile.

--- sync.sh
+++ sync.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-. "$(dirname "$0")/nginx_conf_gen.sh"
 . "$(dirname "$0")/sync_project.sh"
 
 if [ -z "$DOMAIN" ]; then
@@ -13,7 +12,6 @@ INTERVAL_SECONDS=${INTERVAL_SECONDS:-120}
 CONFIG_FILE=${CONFIG_FILE:-"/projects.json"}
 
 while true; do
-    has_run=false
     if [ -f "$CONFIG_FILE" ]; then
         num_projects=$(jq '. | length' "$CONFIG_FILE")
         i=0
@@ -24,7 +22,6 @@ while true; do
             if [ "$repo_url" != "null" ]; then
                 [ -z "$base_path" ] && base_path=$(basename "$repo_url" .git)
                 sync_project "$repo_url" "$base_path" "$branch_regex" "proj_$i"
-                has_run=true
             fi
             i=$((i + 1))
         done
@@ -32,13 +29,8 @@ while true; do
         current_base_path="$BASE_PATH"
         [ -z "$current_base_path" ] && current_base_path=$(basename "$REPO_URL" .git)
         sync_project "$REPO_URL" "$current_base_path" "$BRANCH_REGEX" "default"
-        has_run=true
     else
         echo "[$(date +'%H:%M:%S')] No configuration found."
     fi
-    if [ "$has_run" = true ]; then
-        generate_nginx_conf
-    fi
     sleep "$INTERVAL_SECONDS"
 done
-
